01f1b3e3cc48be8d4bb29cf9f7dc7917f2c34565
Logan Knecht <lknecht@fredhutch.org>
2020-04-03 16:52:00 -0700

    Add debug shiny server

--- electron_faust_nextflow_desktop/app/shiny_apps/start_shiny_app.r
+++ electron_faust_nextflow_desktop/app/shiny_apps/start_shiny_app.r
@@ -5,7 +5,9 @@ print("THIS IS WHERE I WOULD START SHINY")
 # runApp("./faust_tools/inst/FAUSTApp/app.R")
 # print(normalizePath(getwd()))
 # "/Users/lknecht/Repositories/FAUST_Nextflow_Desktop/electron_faust_nextflow_desktop"
-faust_shiny_app_file_path <- file.path(getwd(), "app", "shiny_apps", "faust_tools", "inst", "FAUSTApp", "app.R")
+# faust_shiny_app_file_path <- file.path(getwd(), "app", "shiny_apps", "faust_tools", "inst", "FAUSTApp", "app.R")
+faust_shiny_app_file_path <- file.path(getwd(), "app", "shiny_apps", "debug_server", "app.R")
 print(faust_shiny_app_file_path)
-runApp(faust_shiny_app_file_path)
+# SHINY_PORT
+runApp(faust_shiny_app_file_path, port = 10005)
 
